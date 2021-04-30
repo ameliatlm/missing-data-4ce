07dc8e9bed1bd9a7f85d80fe43930f8db27eb6ae
lelaboratoire <grixor@gmail.com>
2021-04-29 22:30:16 -0400

    add utility functions

---
+++ R/utils.R
@@ -0,0 +1,17 @@
+concat <- function(x, y){
+  paste0(x, ' (', round(y, 3)*100, '%)')
+}
+
+concat_median <- function(med, mi, ma){
+  paste0(med, ' [', mi, ', ', ma, ']')
+}
+
+concat_mean <- function(mea, s, acc = 0){
+  paste0(round(mea, acc), ' (', round(s, acc), ')')
+}
+
+right_join0 <- function(x, y, fill = 0L, ...){
+  z <- right_join(x, y, ...)
+  tmp <- setdiff(names(z), names(y))
+  tidyr::replace_na(z, setNames(as.list(rep(fill, length(tmp))), tmp))
+}
